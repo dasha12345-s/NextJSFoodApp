ce68f34480f087bbba8f796814b1260b75fdbed0
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-05-04 14:39:52 -0400

    Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,32 @@
-export default function ({params}){
+import Image from 'next/image'
+
+import { getMeal } from '@/lib/meals'
+import classes from './page.module.css'
+
+export default function Detail ({params}){
+  const meal = getMeal(params.slug);
+
+  meal.instructions = meal.instructions.replace(/\n/g,'<br />')
+
 return(
+  <>
+  <header className={classes.header}>
+    <div className={classes.image}>
+  <Image src={meal.image} alt={meal.title}fill/>
+    </div>
+    <div className={classes.headerText}>
+      <h1>{meal.title}</h1>
+      <p className={classes.creator}>
+        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+      </p>
+      <p className={classes.summary}>{meal.summary}</p>
+    </div>
+  </header>
   <main>
-    <h1>SOME slug</h1>
-    <p>{params.slug}</p>
+    <p className={classes.instructions} dangerouslySetInnerHTML={{
+      __html: meal.instructions,
+    }}></p>
   </main>
+  </>
 )
 }
