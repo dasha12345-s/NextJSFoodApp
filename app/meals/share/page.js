638e67006d462cb760fc7c2e02780b1414926288
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-05-07 16:37:05 -0400

    Working with Server Action Responses & useFormState

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,14 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
 import MealsSub from '@/components/meals/meals-form-sub';
 
 export default function ShareMealPage() {
+  const [state, formAction] =useFormState(shareMeal, {message: null});
 
   return (
     <>
@@ -14,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +48,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label='Your image' name='image' />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsSub />
           </p>
