27771d04ff0afbcadc693f50e9f8bb363223c532
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-05-08 18:19:09 -0400

    Adding Static Metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import Link from 'next/link'
 import { getMeals } from '@/lib/meals'
 import { Suspense } from 'react';
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.'
+}
+
 async function Meals(){
   console.log('DDD')
   const meals = await getMeals(); 
